58bec60fc30776ebee184e597d563f9c2468ed26
Fadli_3312301006 <fadliadtyas10@gmail.com>
2024-05-17 11:42:58 +0700

    first commit

---
+++ resources/views/list_produk.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.7.3/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="overflow-x-auto">
+        <div class="ml-10 mt-20">
+            <table class="table">
+                <thead>
+                    <tr class="bg-base-200">
+                        <th>No</th>
+                        <th>Nama Produk</th>
+                        <th>Deskripsi Produk</th>
+                        <th>Harga Produk</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($nama as $index => $item)
+                    <tr class="bg-base-200">
+                        <td>{{$index + 1}}</td>
+                        <td>{{ $item}}</td>
+                        <td>{{ $desc[$index]}}</td>
+                        <td>{{ $harga[$index]}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+</body>
+</html>
